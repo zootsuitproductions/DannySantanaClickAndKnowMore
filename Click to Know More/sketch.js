ba9c6abdce890c08d0e3f6f71609d19c6b3646fe
zootsuitproductions <dannysantanasf@gmail.com>
2017-12-09 03:16:10 -0800

    Update sketch.js

--- Click to Know More/sketch.js
+++ Click to Know More/sketch.js
@@ -3,17 +3,24 @@
 var xPosition = 100
 var myP;
 var counter1 = 0
-var firstTime = true
 var switchTabButton;
 var ethicsYTranslationFactor = 500;
 var pos = 0;
-
+var headerColor = [39, 163, 216];
+var backgroundColor = [255,255,255];
+//use the following code to make this color: (backgroundColor[0],backgroundColor[1],backgroundColor[2])
+var image;
 
 var webPages = {
+  //the current tab is the page the user is on, which is dynamic, and the tabs count is the amount, -1, of tabs, so I can cycle through the tabs.
   tabsCount: 2,
   currentTab: 2,
   tabsArray: [[1000,500,100,100,100], [1000,500,200,0,0]],
   loadTab: function() {
+    //allows cycling between tabs through one button
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    pos = 0
+    //makes the page go to the top, and resents the scrolling position so the top header will be at the top
     print(webPages.currentTab)
     clear();
     if (webPages.currentTab == webPages.tabsCount) {
@@ -31,10 +38,12 @@ var webPages = {
 }
 
 function setup() {
+  image = createImg('1.jpg');
+  image.position(0,300)
+  image.size(windowWidth,windowHeight*2-600)
   angleMode(DEGREES);
   //myP = createP('Should the self-driving car prioritize the life of the driver, or should it reduce the loss of life at the expense of the driver?');
   createCanvas(windowWidth,windowHeight*2-300);
-  background(200);
   //makes the button for the straight option
   stayButton.button = createButton(stayButton.text);
   stayButton.button.position(xPosition-20, 500+ethicsYTranslationFactor);
@@ -76,6 +85,8 @@ function setup() {
   checkbox1.position((canvasWidth/2)-260-abs(input1*50),input1Line((canvasWidth/2)-250));
   checkbox1.changed(myCheckedEvent1);
   checkbox1.hide()
+
+  //defines and positions UI elements, including buttons, sliders, and check boxes
 }
 
 var swerveStayResults = {
@@ -98,13 +109,10 @@ var stayButton = {
         ethicsPage.updateTheCarLocation(false);
         fill(255,0,0);
         strokeWeight(1);
-        makeX(xPosition+15,counter1/2+35)
+        ethicsPage.makeX(xPosition+15,counter1/2+35)
         ethicsResultsGraph.updateGraph("stay")
         translate(0,-ethicsYTranslationFactor);
         alert('The driver died in the accident')
-        //this.resetToggled = true
-        //this.text = "Reset"
-        //this.button = createButton(this.text);
       }
     }
   }
@@ -120,8 +128,8 @@ var swerveButton = {
       ethicsPage.updateTheCarLocation(true);
       fill(255,0,0);
       strokeWeight(1);
-      makeX(xPosition+80,480)
-      makeX(xPosition+70,480)
+      ethicsPage.makeX(xPosition+80,480)
+      ethicsPage.makeX(xPosition+70,480)
       ethicsResultsGraph.updateGraph("swerve")
       translate(0,-ethicsYTranslationFactor);
       alert('The two pedestrians died in the accident')
@@ -131,6 +139,7 @@ var swerveButton = {
 
 var car = {
   makeCarBody: function(x,y) {
+    //makes a car-looking box
     fill(255,0,0);
     rect(x,y/2,20,50), rect(x,y/2+10,20,30)
     fill(0);
@@ -150,108 +159,33 @@ var ethicsResultsGraph = {
     print(selected)
     push();
     translate(this.x,this.y);
+    //calculates and displays user answers
     let theStayPercent = (swerveStayResults.stay/(swerveStayResults.stay + swerveStayResults.swerve))*100
     print(theStayPercent)
-    makeGraph(theStayPercent, 100 - theStayPercent);
+    ethicsPage.makeTheGraph(theStayPercent, 100 - theStayPercent);
     //(100 - stayPercent gives the swerve percent)
   }
 };
 
-function makeGraph(stay,swerve) {
-  fill(0,255,255);
-  strokeWeight(0.5);
-  for (var i = 0; i > -1*stay; i-= 2) {
-    rect(0,i,10,2)
-  }
-  fill(0,0,255);
-  for (var y = 0; y > -1*swerve; y-= 2) {
-    rect(20,y,10,2)
-  }
-}
-
-function makeX(x,y) {
-  push()
-  translate(x+1,y-5);
-  //translates so that middle of the x is the coordinate.
-  rotate(45);
-  rect(0,0,2,10)
-  rect(-4,4,10,2)
-  pop()
-}
-
-/*function updateCarLocation(swerveBool) {
-  if (counter1 >= 700 && firstTime == true && counter1 < 800) {
-    firstTime = false
-    alert('Self-Driving cars will not make urgent decisions in an accident, like humans. They decide based on machine learning and their programming. Take your time.')
-  }
-  if (counter1 < 700) {
-    strokeWeight(0);
-    background(255);
-    fill(230,200,80)
-    rect(xPosition-20,480,60,5)
-    //makes barricade
-    fill(255,230,150);
-    strokeWeight(0.5)
-    ellipse(xPosition+80,480,5,5)
-    ellipse(xPosition+70,480,5,5)
-    strokeWeight(0)
-    fill(0,255,255);
-    drawArrow()
-    fill(0,0,255)
-    push()
-    translate(xPosition+5,410)
-    rotate(-20)
-    translate(-xPosition+30,-400)
-    drawArrow()
-    pop()
-    if (swerveBool) {
-      push()
-      //translate(xPosition,counter1)
-      rotate(-10)
-      car.makeCarBody(xPosition-40,counter1+20)
-      pop()
-    } else {
-      car.makeCarBody(xPosition,counter1)
-    }
-    strokeWeight(0)
-    counter1 += 3
-  }
-}*/
-
 var ethicsPage = {
+  //this is one of the tabs, that involves an ethics game
   makeText: function(yTranslation) {
-    fill(120,150,70);
+    fill(255);
+    rect(0,0,windowWidth,500);
+    fill(headerColor[0],headerColor[1],headerColor[2]);
     textSize(25);
     text("Ethical Dillemas of AI",30,100);
     textSize(16);
     fill(0);
     text("One ethical problem of AI arises visibly in the field of self-driving cars. In a scenario where a crash is inevitable, what should the car do? This question is fascinating \nbecause AI can be trained to act and make calculated decisions, unlike those of a human in a spontaneous situation. Here's another fascinating thought: if we declare \nthe purpose of self-driving cars as to save as many lives as possible, should it sacrifice the passengers in order to save strangers? This is a huge ethical issue, \nand even affects the economic success of the cars, decreasing the amount of people using them, and thus decreasing the ammount of lives saved. \nBy priotizing the life of the driver, some believe that more lives would be saved because more people would feel comfortable in the cards. Wouldn't it be weird to know \nthat your car didn't care about saving your life in particular? The following demonstration replicates a scenario in which a self-driving car must kil to save life.",60,120);
-    fill(200,0,10);
+    fill(headerColor[0],headerColor[1],headerColor[2]-40);
     textSize(25);
     push()
     translate(windowWidth/2-500,250)
     rotate(90);
-    text("Scroll ",-140,110)
-    //text("Scroll down for a controversial ethical scenario.",-140,110)
-    rotate(-10);
-    text("        down",-140,100)
-    rotate(-10);
-    text("                for",-140,100)
-    rotate(-10);
-    text("                    a",-140,100)
-    rotate(-10);
-    text("                      controversial",-140,100)
-    rotate(-10);
-    text("                                          ethical",-140,130)
-    rotate(-10);
-    text("                                                    scenario",-140,160)
-    rotate(-10);
-    text("                                                                 example.",-140,210)
-    //makes curving text
+    text("------------>",-140,110)
     pop()
-    //text("The ethical problems of AI arise in accountability; in deep learning programs, the reasoning behind the machine's decisions is impossible to explain for humans. \nThis becomes a legal issue, as the decisions of the AI are no longer taught by the programmer.",60,120);
-
-
+    text("Scroll down for a controversial scenario.",25,250)
   },
   makeX: function(x,y) {
     push()
@@ -263,10 +197,20 @@ var ethicsPage = {
     pop()
   },
   makeTheGraph: function(stay,swerve) {
+    //makes text that informs the user about ethical issues
+    fill(0);
+    text("As you can see, the decisions that AI will have to make are daunting, \nand people may have different ideas about what the AI should do. \nClick the top button to on to the next page and learn about how AI works.",100,-90)
+    textSize(10);
+    text("Responses",-10,10)
+    textSize(7);
+    text("__0%",32,2);
+    text("__50%",32,-48);
+    text("__100%",32,-98);
     fill(0,255,255);
     strokeWeight(0.5);
     for (var i = 0; i > -1*stay; i-= 2) {
       rect(0,i,10,2)
+
     }
     fill(0,0,255);
     for (var y = 0; y > -1*swerve; y-= 2) {
@@ -278,14 +222,11 @@ var ethicsPage = {
     triangle(xPosition+10,475,xPosition-10,455,xPosition+30,455)
   },
   updateTheCarLocation: function(swerveBool){
-    if (counter1 >= 700 && firstTime == true && counter1 < 800) {
-      firstTime = false
-      //alert('Self-Driving cars will not make urgent decisions in an accident, like humans. They decide based on machine learning and their programming. Take your time.')
-    }
+    //checks if the counter has not maxed out yet
     if (counter1 < 700) {
       strokeWeight(0);
-      background(201, 180, 126);
-      fill(100,100,190);
+      background(backgroundColor[0],backgroundColor[1],backgroundColor[2]);
+      fill(headerColor[0],headerColor[1],headerColor[2]);
       textSize(20);
       text("A self-driving car housing 1 person perceives a physical obstacle ahead (a barricade), and two pedestrians crossing a street.",10,300-counter1/7)
       text("The only two options are to either hit the barrier or hit the pedestrians.",10,330-counter1/7)
@@ -346,26 +287,34 @@ var ethicsPage = {
 var carBackgroundHasBeenCreated = false;
 
 function draw() {
+  strokeWeight(0);
   //updateCarLocation()
   if (webPages.currentTab == 0) {
+
+    image.hide()
     slider0.hide()
     slider1.hide()
     checkbox0.hide()
     checkbox1.hide()
+    ethicsPage.makeText(ethicsYTranslationFactor);
     if (pos > 100) {
       translate(0,500)
       ethicsPage.updateTheCarLocation()
       translate(0,-500)
     } else {
       if (carBackgroundHasBeenCreated == false) {
-        background(201, 180, 126);
+        //there is no need to repeatedly make the car, so this variable
+        background(backgroundColor[0],backgroundColor[1],backgroundColor[2]);
         ethicsPage.makeTheCarBody(100,1000)
         carBackgroundHasBeenCreated = true
+      } else {
+        ethicsPage.makeText(ethicsYTranslationFactor);
       }
     }
-    ethicsPage.makeText(ethicsYTranslationFactor);
+
   } else if (webPages.currentTab == 1) {
     carBackgroundHasBeenCreated = false
+    image.hide()
     slider0.show()
     slider1.show()
     checkbox0.show()
@@ -374,12 +323,30 @@ function draw() {
     makeNeuronText()
   } else if (webPages.currentTab == 2) {
     loadMainPage()
+    image.show()
     slider0.hide()
     slider1.hide()
     checkbox0.hide()
     checkbox1.hide()
   }
-  makeTopHeader()
+  topBarStaysAtTopOfView(webPages.currentTab)
+}
+
+var lastPosition = 0
+//remembers where the header was last, to avoid weird/broken animations
+
+function topBarStaysAtTopOfView(windowIndex) {
+  //top bar stays at top of screen
+  if (windowIndex == 2) {
+    topBar.makeTopHeader(0,windowIndex);
+  } else {
+    if (pos >= 0 && pos <= 400) {
+      topBar.makeTopHeader(pos,windowIndex);
+      lastPosition = pos;
+    } else {
+      topBar.makeTopHeader(lastPosition,windowIndex);
+    }
+  }
 }
 
 function drawArrow() {
@@ -387,19 +354,26 @@ function drawArrow() {
   triangle(xPosition+10,475,xPosition-10,455,xPosition+30,455)
 }
 
+///
+///
+///
 ///// main page
 
 function loadMainPage() {
+  image.hide()
+  image = createImg('1.jpg')
+  image.size(windowWidth,windowHeight-321)
   createCanvas(windowWidth,windowHeight*2-300);
-  background(201, 180, 126);
-  fill(120,150,70);
+  background(backgroundColor[0],backgroundColor[1],backgroundColor[2]);
+  fill(headerColor[0],headerColor[1],headerColor[2]);
   textSize(30);
-  text("What is Aritifical Intelligence?",0,110);
+  text("What is Aritifical Intelligence?",20,110);
   textSize(15);
   fill(0);
-  text("AI is most used in natural language processing, identifying objects (perception), and autonomous robots in manufacturing. It is also being implemented \nin the car industry by Google and other companies with self-driving cars. Artificial intelligence is also starting to expand into new fields, \nsuch as medicine, via expert systems. Expert systems accomplish decision-making in medicine or government fields like a human expert. \nIn 2005, researchers at a hospital in New York trained a deep learning program with data from over 700,000 patients, \nand it was able to predict diseases accurately by recognizing patterns in the data.",50,220);
+  text("AI is most used in natural language processing, identifying objects (perception), and autonomous robots in manufacturing. It is also being implemented \nin the car industry by Google and other companies with self-driving cars. Artificial intelligence is also starting to expand into new fields, \nsuch as medicine, via expert systems. Expert systems accomplish decision-making in medicine or government fields like a human expert. \nIn 2005, researchers at a hospital in New York trained a deep learning program with data from over 700,000 patients, \nand it was able to predict diseases accurately by recognizing patterns in the data.",20,200);
   textSize(25);
-  text("Artificial intelligence, or AI, is the process of computers making decisions and predictions \nby mimicking the cognitive abilities of natural intelligence, which humans possess.",20,150);
+  text("Artificial intelligence, or AI, is the process of computers making decisions and predictions \nby mimicking the cognitive abilities of natural intelligence, which humans possess.",20,140);
+
 }
 
 //NEURON EXPLANATION
@@ -408,7 +382,6 @@ function loadMainPage() {
 //
 
 var i;
-
 var slider0;
 var slider1;
 var checkbox;
@@ -492,8 +465,8 @@ function doNeuron() {
   clear()
   weight0 = slider0.value()/100;
   weight1 = slider1.value()/100;
-  createCanvas(canvasWidth,canvasHeight)
-  background(201, 180, 126)
+  createCanvas(canvasWidth,windowHeight+400)
+  background(backgroundColor[0],backgroundColor[1],backgroundColor[2])
   loadNeuron()
   fill(0);
   if (i < canvasWidth/2) {
@@ -517,45 +490,78 @@ function doNeuron() {
   i ++
   //animates the inputs going into the neuron
 }
+
+var topBar = {
+  makeTopHeader: function(y,tab){
+    strokeWeight(0);
+    fill(headerColor[0],headerColor[1],headerColor[2]);
+    rect(0,0+y,windowWidth,70);
+    fill(0);
+    //makes bar on top of screen
+    fill(200);
+    strokeWeight(0);
+    fill(0);
+    textSize(30);
+    text("Artificial Intelligence <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <>",10,38+y)
+    if (tab == 2) {
+      switchTabButton.position(0, windowHeight-270+y);
+    } else {
+      switchTabButton.position(0, 51+y);
+    }
+
+  }
+}
+
 //
 function makeTopHeader() {
-  fill(150,150,50);
-  rect(0,0,windowWidth,50);
+  strokeWeight(0);
+  fill(headerColor[0],headerColor[1],headerColor[2]);
+  rect(0,0,windowWidth,70);
   fill(0);
   //makes bar on top of screen
   fill(200);
   strokeWeight(0);
-  rect(0,50,windowWidth,20)
+  //rect(0,50,windowWidth,20)
   //rect(windowWidth/3,50,windowWidth/3,20)
   //rect(2*(windowWidth/3),50,windowWidth/3,20)
   //makes rectangular button places
   fill(0);
   textSize(30);
-  text("Artificial Intelligence",0,38)
+  text("Artificial Intelligence",10,38)
 }
 
 function makeNeuronText() {
   textSize(25);
   fill(0);
+  translate(0,20);
   text("-1: no \n+1: yes",20,110);
-  fill(120,150,70);
-  text("So how does AI work?",30,310);
+  fill(headerColor[0],headerColor[1],headerColor[2]);
+  text("So how does AI work?",20,310);
   textSize(15);
   fill(0);
-  text("Achieving intelligence is accomplished by mimicking biology; a neural network, based on that of a brain, perceives information which stimulates artificial neurons \nuntil a result is achieved. Each artificial neuron is a mathematical function that has 1 or more inputs, and 1 output. \nFor every input, there is also a weight, which allows different inputs to contribute in different amounts to the final 'decision.'\nThese neurons are linked together in complex chains that allow complex decisions to be made.\n\nTHE EXAMPLE replicates a single neuron. Go ahead and experiment with different weights and inputs, and see how the output is affected.\n\nIn AI programs, the neurons are trained by inputing data, and allowing the neuron to guess, and then adjusting the weights based on the actual result.",60,340);
+  text("Achieving intelligence is accomplished by mimicking biology; a neural network, based on that of a brain, perceives information which stimulates artificial neurons \nuntil a result is achieved. Each artificial neuron is a mathematical function that has 1 or more inputs, and 1 output. \nFor every input, there is also a weight, which allows different inputs to contribute in different amounts to the final 'decision.'\nThese neurons are linked together in complex chains that allow complex decisions to be made.\n\nTHE EXAMPLE replicates a single neuron. Go ahead and experiment with different weights and inputs, and see how the output is affected.\n\nIn AI programs, the neurons are trained by inputing data, and allowing the neuron to guess, and then adjusting the weights based on the actual result.",20,340);
+  translate(0,-20);
 }
 
 function mouseWheel(event) {
-  print(event.delta);
+  print(event.deltaY);
   //move the square according to the vertical scroll amount
-  if (pos < 0) {
-    pos = 0
-  } else if (pos > windowHeight*2-300) {
-    pos = windowHeight*2-300
+
+  if (webPages.currentTab != 2) {
+    if (pos < 0) {
+      pos = 0
+    } else if (pos > 400) {
+      pos = 400
+    } else {
+      image.hide()
+      slider0.hide()
+      slider1.hide()
+      checkbox0.hide()
+      checkbox1.hide()
+      pos += event.deltaY;
+    }
+    print(pos)
   } else {
-    pos += event.delta;
+    return false;
   }
-  print(pos)
-  //uncomment to block page scrolling
-  //return false;
 }
